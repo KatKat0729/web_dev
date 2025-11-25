9541d748590a3dba6368ae9349b6daf6aadc181d
KatKat0729 <kat.wightman@ucas-edu.net>
2025-11-25 22:17:40 +0000

    fiished hidden image assignment

--- rats.js
+++ rats.js
@@ -1,5 +1,9 @@
 // JavaScript
 
 function change(){
-    document.getElementById("title").src = "https://www.thesprucepets.com/thmb/XHssoXO9mI_Uj63lZDzPhA4Kwns=/4000x0/filters:no_upscale():strip_icc()/sleeping-rats-120694080-57ffdee53df78cbc2894461a.jpg"
+    document.getElementById("image").style.display = "block";
+}
+
+function hideImage(){
+    document.getElementById("image").style.display = "none";
 }
