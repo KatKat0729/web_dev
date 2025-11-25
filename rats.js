c5f00653427e7453da66f2aba849cd17b29b1c93
KatKat0729 <kat.wightman@ucas-edu.net>
2025-11-25 22:09:41 +0000

    started assignments

---
+++ rats.js
@@ -0,0 +1,5 @@
+// JavaScript
+
+function change(){
+    document.getElementById("title").src = "https://www.thesprucepets.com/thmb/XHssoXO9mI_Uj63lZDzPhA4Kwns=/4000x0/filters:no_upscale():strip_icc()/sleeping-rats-120694080-57ffdee53df78cbc2894461a.jpg"
+}
